79ebb720a8475a4c6052b32504f9937526a80b9a
heather <>
2004-08-02 21:03:21 +0000

    use more appropriate ids

--- src/DefineClids.cxx
+++ src/DefineClids.cxx
@@ -1,9 +1,10 @@
 /** @file DefineClids.cxx
 
-  $Header: /nfs/slac/g/glast/ground/cvs/EbfEvent/src/DefineClids.cxx,v 1.1.1.1 2004/03/25 19:48:01 heather Exp $
+  $Header: /nfs/slac/g/glast/ground/cvs/LdfEvent/src/DefineClids.cxx,v 1.1.1.1 2004/05/13 22:02:25 heather Exp $
 */
 #include "GaudiKernel/ClassID.h"
 
-const CLID& CLID_LdfDiagnosticData   = 4100;
-const CLID& CLID_LdfEventSummaryData = 4200;
-const CLID& CLID_LdfTime = 4300;
+const CLID& CLID_LdfDiagnosticData   = 2210;
+const CLID& CLID_LdfEventSummaryData = 2000;
+const CLID& CLID_LdfTime = 2010;
+const CLID& CLID_LdfGem = 2020;
