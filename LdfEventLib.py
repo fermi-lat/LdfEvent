bf5772b3547f12cc11407e3c1e323705686fabd1
jrb <>
2012-08-17 18:47:50 +0000

    patches for Windows no-install-headers

--- LdfEventLib.py
+++ LdfEventLib.py
@@ -1,7 +1,15 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/LdfEvent/LdfEventLib.py,v 1.1 2008/07/09 21:13:42 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['LdfEvent'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'LdfEvent') 
+
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'lsfData') 
+        env.Tool('findPkgPath', package='enums')
+        env.Tool('findPkgPath', package='Event')
+
 
 def exists(env):
     return 1;
--- SConscript
+++ SConscript
@@ -1,5 +1,5 @@
 # -*- python -*-
-# $Header: /nfs/slac/g/glast/ground/cvs/LdfEvent/SConscript,v 1.14 2010/06/12 17:29:41 jrb Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/LdfEvent/SConscript,v 1.15 2012/01/23 19:46:52 jrb Exp $
 # Authors: Heather Kelly <heather@milkyway.gsfc.nasa.gov>
 # Version: LdfEvent-04-12-02
 Import('baseEnv')
@@ -9,6 +9,7 @@ progEnv = baseEnv.Clone()
 libEnv = baseEnv.Clone()
 
 libEnv.Tool('addLinkDeps', package='LdfEvent', toBuild='shared')
+
 LdfEvent = libEnv.SharedLibrary('LdfEvent', listFiles(['src/*.cxx']))
 
 progEnv.Tool('LdfEventLib')
